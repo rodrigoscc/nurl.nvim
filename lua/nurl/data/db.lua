cf97e0d67b486af66cdfe454ff4f1d74dc076cf0
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2025-11-29 14:06:13 -0500

    chore: missing indent fix

--- lua/nurl/data/db.lua
+++ lua/nurl/data/db.lua
@@ -7,9 +7,9 @@ ffi.cdef([[
     int sqlite3_open(const char *filename, sqlite3 **ppDb);
     int sqlite3_close(sqlite3*);
     int sqlite3_exec(
-      sqlite3*, const char *sql, int (*callback)(void*,int,char**,char**), void*, char **errmsg);
+        sqlite3*, const char *sql, int (*callback)(void*,int,char**,char**), void*, char **errmsg);
     int sqlite3_prepare_v2(
-      sqlite3*, const char *zSql, int nByte, sqlite3_stmt **ppStmt, const char **pzTail);
+        sqlite3*, const char *zSql, int nByte, sqlite3_stmt **ppStmt, const char **pzTail);
     int sqlite3_reset(sqlite3_stmt*);
     int sqlite3_step(sqlite3_stmt*);
     int sqlite3_finalize(sqlite3_stmt*);
